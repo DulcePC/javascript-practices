7371942f1bdb985766fe71f6a5702088d6dc1ae8
Dulce Perez <d.perez@kation.com.do>
2021-01-07 17:19:35 -0400

    Testing foreach and filter loops

---
+++ filter.js
@@ -0,0 +1,38 @@
+const companiesFilter = [
+  {name: "Kation Interactive", category: "Development", start:2019, end:"undefined"},
+  {name: "Mctekk", category: "Development", start:2019, end:"undefined"},
+  {name: "LeSoluciones", category: "Secretary", start:2015, end:2019},
+  {name: "DulcePc", category: "dont know that the fuck it is", start:2015, end:"undefined"},
+]
+
+const ageFilter = [12,45,60,50,2,10,35,25,65,89,34,09,23,47,46];
+
+console.log("");
+
+//Filter thins out from array
+let canDrink = [];
+
+//this its with for loop
+for (let i = 0; i < ageFilter.length; i++) {
+  if(ageFilter[i] > 18) {
+    console.log(ageFilter[i]);
+    canDrink.push(ageFilter);
+    console.log(canDrink);
+  }
+}
+
+console.log("");
+//this its with filter
+console.log("this its filter")
+const canDrinks = ageFilter.filter(function(age){
+  if(age > 18) {
+    return true;
+  }
+})
+console.log(canDrinks);
+
+console.log("");
+
+console.log("clean the code")
+const canDrinkClean = ageFilter.filter(age => age > 18);
+console.log(canDrinkClean);
---
+++ forEach.js
@@ -0,0 +1,18 @@
+const companies = [
+  {name: "Kation Interactive", category: "Development", start:2019, end:"undefined"},
+  {name: "Mctekk", category: "Development", start:2019, end:"undefined"},
+  {name: "LeSoluciones", category: "Secretary", start:2015, end:2019},
+  {name: "DulcePc", category: "dont know that the fuck it is", start:2015, end:"undefined"},
+]
+
+const age = [12,45,60,50,2,10,35,25,65,89,34,09,23,47,46];
+
+//You can create element
+//you can take index
+//you can take all array
+console.log("");
+companies.forEach(function(company){
+  console.log(company.name);
+  console.log(company);
+
+})
