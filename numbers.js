5eb5e0f80d75f169ee08bf328ac27d338abcbf0a
Dulce Perez <dulcefrancelispc@gmail.com>
2021-01-13 20:10:10 -0400

    Numbers examples

---
+++ numbers.js
@@ -0,0 +1,38 @@
+//Creacion de numeros
+let numero1 = 1;
+let numero2 = "2"; //eso es un string
+let numero3 =  40.39;
+let numero4 = -40;
+//Todos los numeros se crean de la misma forma
+
+//Operaciones en javascript
+let resultado;
+resultado = numero1 + numero2;
+console.log(resultado);
+
+resultado = numero2 * numero4;
+console.log(resultado);
+
+resultado = numero2 / numero2;
+console.log(resultado);
+//modulo
+resultado = numero3 % 30;
+console.log(resultado);
+
+resultado = Math.floor(numero3);
+console.log(resultado);
+
+//Orden en que se ejecutan las operaciones.
+//multiplicacion y diviciones son primero -- luego suma y restas
+
+//Incrementos y decrementos
+let puntaje = 5;
+let puntaje2 = 5;
+console.log(puntaje++);
+console.log(puntaje++);
+console.log(puntaje2--);
+console.log(puntaje2--);
+
+
+
+
