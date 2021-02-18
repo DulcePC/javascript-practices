fb99751138896d2acb5c7983b632b2798054adab
Dulce Perez <dulcefrancelispc@gmail.com>
2021-02-17 22:51:55 -0400

    start class with functions

--- functions.js
+++ functions.js
@@ -14,3 +14,35 @@ function saludos(name,lastName,range,greetingType) {
 
 console.log(saludos("Dulce","Pérez","esclava","Bienvenida"));
 console.log("");
+
+function sumar() {
+    console.log(2+2);
+}
+
+sumar();
+
+//function expression - funcion de expresion
+
+const sumar2 = function() {
+    console.log(3 + 5)
+}
+
+sumar2();
+
+//Diferencia entre function expression y declaracion de funcion:
+//Bueno es que la declaracion de funcion puedes llamar la funcion incluso cuando la ejecutas antes de declarar la funcion a diferencia de expresion de funcion
+
+//Diferencia entre funciones y metodos:
+//Hacen lo mismo lo unico que lo hace cambiar es la manera en que lo llamas 
+const numero1 = 20;
+const numero2 = "20";
+
+console.log(parseInt(numero2)); //esto es una function
+console.log(numero1.toString()); //esto es un metodo
+
+function saludar(nombre, apellido = 'no tiene apellido') { //nombre y apellido son parametros
+    console.log( `Hola ${nombre} ${apellido}`);
+}
+
+saludar('dulce',"perez"); //argumentos
+saludar('dulce'); //parametros por default cuando en el parametro le agregas un valor, si falta un argumento al llamar la funcion
