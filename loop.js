eaf2a1b0eea40d8b01814adfff5353443f8c34e9
Dulce Perez <dulcefrancelispc@gmail.com>
2021-03-03 20:28:11 -0400

    Start loop module

--- loop.js
+++ loop.js
@@ -6,17 +6,25 @@ console.log(friendsYourParty);
 
 
 let dulce = 22;
-// while (dulce > 22) {
-//     console.log("sigue haciendo lo que tu quiera mi crazy");
-// }
-
 while(dulce <= 18) {
     console.log("tu no puede hacer lo que se te venga en gana no mi crazy im sorry");
 }
 
-
 let totalSerrucho = 10;
-
 for (serrucho = 0; serrucho < totalSerrucho; serrucho++ ) {
     console.log("ya tenemos pal wendy");
 }
+
+for (let i = 0; i <= 20; i++) {
+    console.log('numero' + ' ' + i);
+}
+
+//Pares y impares
+for (let i = 0; i <= 20; i++) {
+    if( i % 2 === 0 ) {
+        console.log(`El numero ${i} es par`)
+    } else {
+        console.log(`El numero ${i} es impar`)
+    }
+}
+
