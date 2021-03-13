d37e98822d9bf495b8787b247e9203aed6a65f30
Dulce Perez <dulcefrancelispc@gmail.com>
2021-03-13 08:48:15 -0400

    Continuacion del estudio de dom

--- dom.js
+++ dom.js
@@ -16,3 +16,21 @@ console.log(elemento3);
 //QuerySelector = devuelve el primero que encuentre ( clases y id)
 let elemento4 = document.querySelector('.card');
 console.log(elemento4);
+
+let titulo = document.querySelector('h1');
+console.log(titulo);
+console.log(titulo.innerText);   // si en el css tiene un visibility hidden, este no lo va a encontrar
+console.log(titulo.textContent); // por el contrario este si
+console.log(titulo.innerHTML);   // este otro se trae el html
+
+console.log(titulo.textContent = 'Nuevo titulo');
+
+let imagen = document.querySelector('img');
+imagen.src = 'placeholder-img-dos.png';
+
+// cambiar estilos con css
+imagen.style.height = '250px';
+
+const cardBody = document.querySelector('.card-body');
+console.log(cardBody);
+cardBody.classList.add('bg-info', 'd-flex');
