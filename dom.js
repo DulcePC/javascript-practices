f03bb4e24824b3500749e417027f40bea04e87d1
Dulce Perez <dulcefrancelispc@gmail.com>
2021-03-17 10:06:34 -0400

    Creando html con js

--- dom.js
+++ dom.js
@@ -20,7 +20,7 @@ console.log(elemento4);
 let titulo = document.querySelector('h1');
 console.log(titulo);
 console.log(titulo.innerText);   // si en el css tiene un visibility hidden, este no lo va a encontrar
-console.log(titulo.textContent); // por el contrario este si
+console.log(titulo.textContent); // por el contrario este si y agregarlo
 console.log(titulo.innerHTML);   // este otro se trae el html
 
 console.log(titulo.textContent = 'Nuevo titulo');
@@ -41,3 +41,18 @@ console.log(elemento5.childNodes); // cuenta los espacios como elementos html
 console.log(elemento5.children); // lo contrario
 console.log(elemento5.parentNode); // el padre del elemento
 
+const elemento6 = document.querySelector('.btn-primary');
+console.log(elemento6);
+elemento6.remove();
+
+//creando enlace ( creando elementos con js)
+const enlace = document.createElement('a');
+// agregando texto
+enlace.textContent = "Nuevo enlace";
+// direccion de enlace
+enlace.href = 'https://github.com/';
+enlace.target = "_blank";
+enlace.classList.add('bg-warning', 'p-2', 'd-block')
+//Mostrar enlace
+document.querySelector('.d-flex').appendChild(enlace);
+console.log(enlace);
