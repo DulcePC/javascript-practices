9c692cece681675b0e1efcfdee5057fdf7910d19
DulcePC <dulcefrancelispc@gmail.com>
2022-05-23 10:34:19 -0400

    Feat: :sparkles: finish page about variables

--- js/variables.js
+++ js/variables.js
@@ -1,28 +1,17 @@
-//Las var se pueden reasignar;
-//Las nombres de las variables no pueden iniciar con numeros.
 var producto = "Monitos de 24 Pulgadas";
-console.log(producto);
-
-//Se pueden inicializar sin valor y asignarlo despues
 var disponible;
-disponible = true;
-
+disponible = true
 //Inicializar multiples variables
 var categoria = "Computadoras",
     marca = "Marca famosa",
     calificacion = 5;
 
-//camelCase
-var nombreProducto;
-
-//PascalCase
-var NombreProducto;
-
-//Let
-//Puedes reasignar el valor
 let nombre = "Dulce";
-
-//Const
-//No puede ser reasignado
-//las const deben inicializar con un valor, no pueden iniciar vacias.
 const apellido = "Perez";
+
+let saludar = "dice hola";
+if(true) {
+  let saludar =  "dice hola tambien";
+  console.log(saludar); // "dice hola tambien"
+}
+console.log(saludar); // "Dice hola"
